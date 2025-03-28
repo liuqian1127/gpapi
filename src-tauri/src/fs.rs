abaa5efcf0128a40378428ad05f2d123fcbebf65
liu.qian <liuqian0410@163.com>
2025-03-28 11:07:17 +0800

    明确引用减少隐患

--- src-tauri/src/fs.rs
+++ src-tauri/src/fs.rs
@@ -1,8 +1,6 @@
-use serde::{Deserialize, Serialize};
-use std::fs::File;
 use std::{fs, io, path};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TreeNode {
     label: String,
@@ -11,7 +9,7 @@ pub struct TreeNode {
     path: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Setting {
     root_path: String,
@@ -44,7 +42,7 @@ pub fn tree(path: &str) -> Result<TreeNode, String> {
 /// 读取配置
 #[tauri::command]
 pub fn read_setting() -> Result<Setting, String> {
-    let file = File::open(SETTING_FILEPATH);
+    let file = fs::File::open(SETTING_FILEPATH);
     let file = match file {
         Ok(f) => f,
         Err(e) => return Err(format!("读文件失败 [{e}]")),
@@ -66,11 +64,10 @@ pub fn write(path: &str, content: &str) -> Result<String, String> {
     }
 
     let result = fs::write(path, content);
-    if let Err(e) = result {
-        return Err(format!("写入失败 {e}"));
+    match result {
+        Ok(()) => Ok("写入成功".into()),
+        Err(e) => Err(format!("写入失败 {e}")),
     }
-
-    Ok("写入成功".into())
 }
 
 /// 读文件[`path`]全部内容
@@ -113,18 +110,18 @@ fn mkdir(path: &str) -> Result<String, String> {
 }
 
 /// 创建[`path`]文件
-fn create(path: &str) -> Result<File, String> {
+fn create(path: &str) -> Result<fs::File, String> {
     // 确保父目录存在
     let p = path::Path::new(path);
     if let Some(parent) = p.parent() {
-        let result = mkdir(parent.to_str().unwrap());
+        let result = mkdir(parent.to_str().unwrap_or_default());
         if let Err(e) = result {
             return Err(format!("创建父目录失败 [{e}]"));
         }
     }
 
     // 创建文件
-    let file = File::create(path);
+    let file = fs::File::create(path);
     match file {
         Ok(f) => Ok(f),
         Err(e) => Err(format!("创建文件失败 {e}")),
@@ -132,13 +129,13 @@ fn create(path: &str) -> Result<File, String> {
 }
 
 /// 递归列举[`path`]文件树
-fn ls(path: &path::Path) -> io::Result<TreeNode> {
+fn ls(path: &path::Path) -> Result<TreeNode, io::Error> {
     let mut node = TreeNode {
         // file_stem 不带扩展名
-        label: path.file_stem().unwrap().to_string_lossy().to_string(),
+        label: path.file_stem().unwrap_or_default().to_str().unwrap_or_default().to_string(),
         children: Vec::new(),
         is_dir: path.is_dir(),
-        path: path.to_string_lossy().to_string(),
+        path: path.to_str().unwrap_or_default().to_string(),
     };
 
     if path.is_dir() {
